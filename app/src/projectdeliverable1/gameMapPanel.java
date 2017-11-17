c5157ae70fc1fd29778b961ced40e425dd616ab8
dsutt13 <dsutt13@104.39.142.133>
2017-11-17 10:25:15 -0500

    fuck yourself

--- app/src/projectdeliverable1/gameMapPanel.java
+++ app/src/projectdeliverable1/gameMapPanel.java
@@ -25,21 +25,45 @@ import javax.swing.JPanel;
 public class gameMapPanel extends JPanel implements ActionListener, KeyListener {
 
     JButton map, pmans, phyrst, champs, madMex, den, cafe, player;
-        int x = 1100;
-        int y = 850;
-     public void paintComponent(Graphics g) {
-        
+    int x = 1100;
+    int y = 850;
+    pmansPanel pmansGame;
+    phyrstPanel phyrstGame;
+
+    public void paintComponent(Graphics g) {
+
         super.paintComponent(g);
         requestFocusInWindow();
         Image img = Toolkit.getDefaultToolkit().getImage("images/map.png");
-        g.drawImage(img,0,0,2000,1000,this);   
-     
+        g.drawImage(img, 0, 0, 2000, 1000, this);
+
     }
     
+    public void intersect() {
+        
+        Rectangle r1 = new Rectangle(1450, 450, 100, 100);//pmans
+        Rectangle r2 = new Rectangle(700, 500, 100, 100);//phyrst
+        Rectangle r3 = new Rectangle(700, 400, 100, 100);//champs
+        Rectangle r4 = new Rectangle(800, 850, 100, 100);//madMex
+        Rectangle r5 = new Rectangle(1650, 200, 100, 100);//den
+        Rectangle r6 = new Rectangle(150, 150, 100, 100);//cafe
+        Rectangle play = player.getBounds();
+        
+        if (play.intersects(r1)) {
+            System.out.println("Fuck");
+            remove(map);
+            add(pmansGame);
+            validate();
+            repaint();
+        }
+    }
+
     public gameMapPanel() {
         super();
         setLayout(null);
         
+        //intersect();
+
         pmans = new JButton("PMans");
         phyrst = new JButton("Phyrst");
         champs = new JButton("Champs");
@@ -48,6 +72,9 @@ public class gameMapPanel extends JPanel implements ActionListener, KeyListener 
         cafe = new JButton("Cafe 210 West");
         player = new JButton();
         
+        pmansGame = new pmansPanel();
+        phyrstGame = new phyrstPanel();
+
         pmans.addActionListener(this);
         phyrst.addActionListener(this);
         champs.addActionListener(this);
@@ -55,15 +82,15 @@ public class gameMapPanel extends JPanel implements ActionListener, KeyListener 
         den.addActionListener(this);
         cafe.addActionListener(this);
         player.addActionListener(this);
-        
-        pmans.setBounds(1450,450,100,100);
-        phyrst.setBounds(700,500,100,100);
-        champs.setBounds(700,400,100,100);
-        madMex.setBounds(800,850,100,100);
-        den.setBounds(1650,200,100,100);
-        cafe.setBounds(150,150,100,100);
-        player.setBounds(x,y,75,75);
-        
+
+        pmans.setBounds(1450, 450, 100, 100);
+        phyrst.setBounds(700, 500, 100, 100);
+        champs.setBounds(700, 400, 100, 100);
+        madMex.setBounds(800, 850, 100, 100);
+        den.setBounds(1650, 200, 100, 100);
+        cafe.setBounds(150, 150, 100, 100);
+        player.setBounds(x, y, 75, 75);
+
         add(pmans);
         add(phyrst);
         add(champs);
@@ -71,52 +98,46 @@ public class gameMapPanel extends JPanel implements ActionListener, KeyListener 
         add(den);
         add(cafe);
         add(player);
-        
+
         setFocusable(true);
         addKeyListener(this);
+        
+
     }
 
-   
-    
     @Override
     public void actionPerformed(ActionEvent ae) {
-        
+
     }
 
     @Override
     public void keyTyped(KeyEvent ke) {
-        
+
     }
 
     @Override
     public void keyPressed(KeyEvent evt) {
-        System.out.println("Key pressed");
+       // System.out.println("Key pressed");
         int kk = evt.getKeyCode();
-        if (kk == evt.VK_LEFT)
-        {
+        if (kk == evt.VK_LEFT) {
             x = x - 10;
         }
-        if (kk == evt.VK_RIGHT)
-        {
+        if (kk == evt.VK_RIGHT) {
             x = x + 10;
         }
-        if (kk == evt.VK_UP)
-        {
+        if (kk == evt.VK_UP) {
             y = y - 10;
         }
-        if (kk == evt.VK_DOWN)
-        {
+        if (kk == evt.VK_DOWN) {
             y = y + 10;
         }
         player.setBounds(new Rectangle(x, y, 75, 75));
+        intersect();
     }
 
     @Override
     public void keyReleased(KeyEvent ke) {
-        
+
     }
-
-
-
 
 }

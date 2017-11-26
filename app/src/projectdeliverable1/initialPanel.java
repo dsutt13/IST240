e4c1bcc3190d78edf27087471f7be79bec901393
dsutt13 <dsutt13@104.39.142.133>
2017-11-26 18:59:31 -0500

    hi

--- app/src/projectdeliverable1/initialPanel.java
+++ app/src/projectdeliverable1/initialPanel.java
@@ -12,7 +12,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JPanel;
+import javax.swing.JPanel; 
 
 /**
  *

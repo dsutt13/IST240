51682381a060e78c798473bbb11252fdd83e0ea1
dsutt13 <dsutt13@104.39.142.133>
2017-11-27 12:51:14 -0500

    mex

--- app/src/projectdeliverable1/mexPanel.java
+++ app/src/projectdeliverable1/mexPanel.java
@@ -6,25 +6,62 @@
 package projectdeliverable1;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.Timer;
 
 /**
  *
  * @author dsutt13
  */
-public class mexPanel extends JPanel {
+public class mexPanel extends JPanel implements ActionListener {
 
     JButton welcome;
     
-    public mexPanel() {
+    Timer tim;
+    JTextArea message = new JTextArea("Time: ");
+    int limit = 0;
+    int delay = 0;
+    int i = 6;
+    initialPanel initial;
+    gameMapPanel map;
+    
+    public mexPanel(initialPanel a) {
         super();
         setLayout(null);
         setBackground(Color.orange);
+        initial = a;
         
         welcome = new JButton("Welcome to Mad Mex");
         welcome.setBounds(1000,0,200,200);
         add(welcome);
+        
+        add(message);
+        message.setBounds(0, 0, 100, 50);
+
+        delay = 1000;
+        tim = new Timer(delay, this);
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        Object obj = ae.getSource();
+        if (obj == tim) {
+            i = i - 1;
+            message.setText("Time: " + i);
+        }
+
+        if (i == 0) {
+            initial.remove(initial.mexGame);
+            initial.add(initial.map);
+            initial.validate();
+            initial.repaint();
+
+        }
     }
     
 }

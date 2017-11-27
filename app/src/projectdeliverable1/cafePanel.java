838079b66ec28028e647e71adaddbab9dc0683cf
dsutt13 <dsutt13@104.39.142.133>
2017-11-27 15:03:06 -0500

    den

--- app/src/projectdeliverable1/cafePanel.java
+++ app/src/projectdeliverable1/cafePanel.java
@@ -60,7 +60,7 @@ public class cafePanel extends JPanel implements ActionListener {
             initial.add(initial.map);
             initial.validate();
             initial.repaint();
-
+            i = 6;
         }
     }
 

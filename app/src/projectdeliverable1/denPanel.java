37dffa72dd4b1165dccbab715489a9f1ed2bbfac
dsutt13 <dsutt13@104.39.142.133>
2017-11-26 20:04:03 -0500

    game panels n'at

---
+++ app/src/projectdeliverable1/denPanel.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectdeliverable1;
+
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author dsutt13
+ */
+public class denPanel extends JPanel {
+
+    JButton welcome;
+    
+    public denPanel() {
+        super();
+        setLayout(null);
+        setBackground(Color.orange);
+        
+        welcome = new JButton("hello there");
+        welcome.setBounds(1000,0,200,200);
+        add(welcome);
+    }
+    
+}

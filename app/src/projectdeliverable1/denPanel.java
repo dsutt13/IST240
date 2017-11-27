adb0c0cbfc7a08d019f8b2fa770762edea9d174f
dsutt13 <dsutt13@104.39.142.133>
2017-11-27 12:51:36 -0500

    den

--- app/src/projectdeliverable1/denPanel.java
+++ app/src/projectdeliverable1/denPanel.java
@@ -6,25 +6,60 @@
 package projectdeliverable1;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.Timer;
 
 /**
  *
  * @author dsutt13
  */
-public class denPanel extends JPanel {
+public class denPanel extends JPanel implements ActionListener {
 
     JButton welcome;
+    Timer tim;
+    JTextArea message = new JTextArea("Time: ");
+    int limit = 0;
+    int delay = 0;
+    int i = 6;
+    initialPanel initial;
+    gameMapPanel map;
     
-    public denPanel() {
+    public denPanel(initialPanel a) {
         super();
         setLayout(null);
         setBackground(Color.orange);
+        initial = a;
         
         welcome = new JButton("Welcome to the Lion's Den!");
         welcome.setBounds(1000,0,200,200);
         add(welcome);
+        
+        add(message);
+        message.setBounds(0, 0, 100, 50);
+
+        delay = 1000;
+        tim = new Timer(delay, this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        Object obj = ae.getSource();
+        if (obj == tim) {
+            i = i - 1;
+            message.setText("Time: " + i);
+        }
+
+        if (i == 0) {
+            initial.remove(initial.denGame);
+            initial.add(initial.map);
+            initial.validate();
+            initial.repaint();
+
+        }
     }
     
 }
